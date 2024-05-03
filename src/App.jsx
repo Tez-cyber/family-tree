aaaad6706c622161dd778cbf66d15cf34e967f2c
Salami Azeez <salamitopeazeez@gmail.com>
2024-05-03 13:32:48 +0100

    Completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react"
-import data from './data.json'
+import Navbar from "./components/Navbar"
+
 
 function App() {
   
   return (
     <>
-     
-    
+     <Navbar />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='bg-black text-white'>
+      <div className="flex justify-between px-7 py-8">
+        <div className="">
+            <h1 className='text-3xl font-bold'>Fam-Tree</h1>
+        </div>
+        <div className='flex justify-center gap-4 items-center'>
+            <button className='bg-white text-black px-4 py-3 rounded'>
+                Create Family
+            </button>
+            <button className='bg-white text-black px-4 py-3 rounded'>
+                Family member
+            </button>
+        </div>
+      </div>
     </div>
   )
 }
