b5a4f56081d86e6b1426ab7854b0211e47a0b3fa
Salami Azeez <salamitopeazeez@gmail.com>
2024-05-04 17:19:22 +0100

    Changed width for displaying families for mobile

--- src/components/Families.jsx
+++ src/components/Families.jsx
@@ -5,11 +5,15 @@ const Families = ({ families }) => {
         <div>
             {
                 families.map((fam, i) => (
-                    <div className="flex my-2 p-4 rounded-md justify-between items-center w-[1024px] mx-auto bg-gray-200 " key={i}>
+                    <div className="flex my-2 p-4 rounded-md justify-between items-center mx-auto bg-gray-200 md:w-[1024px]" key={i}>
                         <div>
                             <p>{fam.lastname}</p>
                             <p className='text-xs text-gray-500'>{fam.origin}</p>
                         </div>
+                        {/* <div>
+                            <p>id</p>
+                            <p className='text-xs text-gray-500'>{fam.id}</p>
+                        </div> */}
                         <div>
                             <button className='bg-black text-white p-2 hover:bg-inherit hover:text-black hover:border hover:border-black hover:rounded-lg'>
                                 See members
