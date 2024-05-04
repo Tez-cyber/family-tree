0abce8c59ae1c007b334089cd8a01c59fbbc842e
Salami Azeez <salamitopeazeez@gmail.com>
2024-05-04 05:14:48 +0100

    Created lastname and origin state for form

--- src/components/registerFamily.jsx
+++ src/components/registerFamily.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Family = () => {
+    const [lastname, setLastName] = useState('')
+    const [origin, setOrigin] = useState('')
     return (
         <div className='w-[300px] mx-auto'>
             <form action="" className=' flex flex-col gap-4 bg-slate-300 mt-12 px-5 py-10'>
                 <div>
                     <p className='font-medium text-[14px]'>Name :</p>
-                    <input type="text" className='border border-black rounded w-full py-1 px-3 outline-none' />
+                    <input type="text" value={lastname} className='border border-black rounded w-full py-1 px-3 outline-none' />
                 </div>
                 <div>
                     <p className='font-medium text-[14px]'>Nationality: </p>
-                    <input type="text" className='border border-black rounded w-full py-1 px-3 outline-none' />
+                    <input type="text" value={origin} na className='border border-black rounded w-full py-1 px-3 outline-none' />
                 </div>
                 <button className='bg-black text-white w-1/2 mx-auto rounded py-2'>
                     Submit
