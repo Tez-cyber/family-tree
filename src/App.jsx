b4f1bb486e825e41a514ac8a968987d5a62fc950
Salami Azeez <salamitopeazeez@gmail.com>
2024-05-04 04:25:22 +0100

    Added families array

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,55 @@ function App() {
   const [registerFamily, setRegisterFamily] = useState(false)
   const handleRegisterFamily = () => {
     setRegisterFamily(prev => !prev)
+    if (registerMember) setRegisterMember(!registerMember)
   }
 
   const [registerMember, setRegisterMember] = useState(false)
   const handleRegisterMember = () => {
     setRegisterMember(prev => !prev)
+    if (registerFamily) setRegisterFamily(!registerFamily)
   }
 
-  
+  const families = [
+    {
+      id: 1,
+      name: "Salami",
+      origin: "Nigeria",
+      members: []
+    },
+    {
+      id: 2,
+      name: "Abdullah",
+      origin: "Saudi",
+      members: []
+    },
+    {
+      id: 3,
+      name: "Vladimir",
+      origin: "Russia",
+      members: []
+    },
+    {
+      id: 4,
+      name: "Danya",
+      origin: "Ghana",
+      members: []
+    },
+    {
+      id: 5,
+      name: "Haruno",
+      origin: "Japan",
+      members: []
+    },
+  ]
+
+
   return (
     <>
-     <Navbar registerFamily={registerFamily} handleRegisterFamily={handleRegisterFamily} handleRegisterMember={handleRegisterMember}/>
-     {registerFamily && <Family />}
-     {registerMember && <Member />}
+      <Navbar registerFamily={registerFamily} handleRegisterFamily={handleRegisterFamily} handleRegisterMember={handleRegisterMember} />
+      {registerFamily && <Family />}
+      {registerMember && <Member />}
+      
     </>
   )
 }
